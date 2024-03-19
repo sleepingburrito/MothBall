0d376a59da977abc09672f8fe53ade76d10edeac
Andrew <emailandrewrodriguez@gmail.com>
2024-03-18 21:50:56 -0500

    adding sv

--- StaticValues.py
+++ StaticValues.py
@@ -1,3 +1,47 @@
+#Holds the static values and magic numbers used during the game. These values should never change during run time.
+import math
+import os
+from enum import IntEnum
+from enum import Enum
+from enum import auto
+
+
+#app info
+#======================
 #Moth Ball
 #Started: 3/18/2024
-#==================
+GAME_NAME = "Moth Ball"
+GAME_VERSION = "0.0.0"
+GAME_DEBUG = True
+
+
+#game units
+#===============
+
+#--spacial--
+
+#--temporal--
+
+
+#miscellaneous
+#======================
+
+#--ect--
+
+#--math--
+
+
+#physics
+#======================
+
+
+#player
+#======================
+
+
+#input
+#======================
+
+
+#maps
+#======================
