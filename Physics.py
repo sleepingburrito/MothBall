b72c1bd2837241f5c1fce7ae2063a78eeb1c2684
Andrew <emailandrewrodriguez@gmail.com>
2024-04-24 22:46:17 -0500

    updated physics

--- Physics.py
+++ Physics.py
@@ -13,21 +13,33 @@ class box:
 
     def ResetAll(self) -> None:
         #locations
-        self.x = 0
-        self.y = 0
-        self.width = 1
-        self.height = 1
+        self._x:float = 0
+        self._y:float = 0
+        self._width:float = 1
+        self._height:float = 1
 
+        self._xCheckpoint:float = 0
+        self._yCheckpoint:float = 0
+
+        #vectors
+        self._xAcceleration:float = 0
+        self._yAcceleration:float = 0
+
+        #flags
+        self.physics:bool = True
+        self.collisionDetection:bool = True 
+    #end of ResetAll(self) 
     
 
     #x, y, width, heigth, top, bottom, left, right
-    @property #X
+    #x axis
+    @property #x
     def x(self) -> float:
         return self._x
     @x.setter
     def x(self, input: float):
         self._x = Tool.ClampValue(input, Sv.GU_ROOM_X, Sv.GU_ROOM_WIDTH)
-        self._right = self._x + self.width
+        self._right = self.x + self.width
     @property #left
     def left(self) -> float:
         return self.x
@@ -40,32 +52,89 @@ class box:
     @width.setter
     def width(self, input: float):
         self._width = input
-        self._right = self._x + self.width
+        self._right = self.x + self.width
     @property #rigth
     def rigth(self) -> float:
         return self._right
     @rigth.setter
     def right(self, input: float):
-        self.x = self.x - self.width
+        self.x = input - self.width
 
-
-    @property
+    #y axis
+    @property #y
     def y(self) -> float:
         return self._y
-    @x.setter
+    @y.setter
     def y(self, input: float):
         self._y = Tool.ClampValue(input, Sv.GU_ROOM_Y, Sv.GU_ROOM_HEIGHT)
-
-    @property
+        self._bottom = self.y + self.height  
+    @property #top
     def top(self) -> float:
         return self.y
     @top.setter
     def top(self, input: float):
         self.y = input
+    @property #heigth
+    def heigth(self) -> float:
+        return self._heigth
+    @heigth.setter
+    def heigth(self, input: float):
+        self._heigth = input
+        self._bottom = self.y + self.heigth
+    @property #bottom
+    def bottom(self) -> float:
+        return self._bottom
+    @bottom.setter
+    def bottom(self, input: float):
+        self.y = input - self.height
+
+    #axis functions
+    def SetXYrelative(self, xy: tuple[float,float]) -> None:
+        self.x += xy[0]
+        self.y += xy[1]
 
 
-    def SetXrelative(self, x: float) -> None:
-        self.x += x
+    #checkpoint
+    @property #x checkpoint
+    def xCheckpoint(self) -> float:
+        return self._xCheckpoint
+    @xCheckpoint.setter
+    def xCheckpoint(self, input: float):
+        self._xCheckpoint = Tool.ClampValue(input, Sv.GU_ROOM_X, Sv.GU_ROOM_WIDTH)
+    @property #y checkpoint
+    def yCheckpoint(self) -> float:
+        return self._yCheckpoint
+    @yCheckpoint.setter
+    def yCheckpoint(self, input: float):
+        self._yCheckpoint = Tool.ClampValue(input, Sv.GU_ROOM_Y, Sv.GU_ROOM_HEIGHT)
 
-    def SetYrelative(self, y: float) -> None:
-        self.y += y
+    #todo make a checkpoint reset that will reset the players postions and reset their volcity etc
+        
+    #Acceleration
+    @property #x Acceleration
+    def xAcceleration(self) -> float:
+        return self._xAcceleration
+    @xAcceleration.setter
+    def xAcceleration(self, input: float):
+        self._xAcceleration = Tool.ClampValue(input, -Sv.ACCELERATION_MAX, Sv.ACCELERATION_MAX)
+    @property #y Acceleration
+    def yAcceleration(self) -> float:
+        return self._yAcceleration
+    @yCheckpoint.setter
+    def yAcceleration(self, input: float):
+        self._yAcceleration = Tool.ClampValue(input, -Sv.ACCELERATION_MAX, Sv.ACCELERATION_MAX)
+
+    def SetAccelerationRelative(self, xy: tuple[float,float]) -> None:
+        self.xAcceleration += xy[0]
+        self.yAcceleration += xy[1]
+
+    def SetAccelerationRelativeVector(self, AngleMagnitude: tuple[float,float]) -> None:
+        tmpVec = Tool.VectorNew(AngleMagnitude[0], AngleMagnitude[1])
+        self.SetAccelerationRelative(tmpVec[0], tmpVec[1])
+
+    def ZeroOutAcceleration(self) -> None:
+        self.xAcceleration = 0
+        self.yAcceleration = 0
+        
+    def GetAccelerationTuple(self) -> tuple[float,float]:
+        return (self.xAcceleration, self.yAcceleration)
--- Tools.py
+++ Tools.py
@@ -7,6 +7,31 @@ def ClampValue(value, minValue, maxValue):
 def CopySign(value):
     return math.copysign(1, value)
 
+#If input is less than +-range it will return zero. Helps round down values close to zero.
+def RoundToZero(input, rang):
+    return 0 if abs(input) < rang else input
+
 #Returns bool if its between two values, inclusionary.
 def IsBetween(value, minValue, maxValue):
     return maxValue >= value >= minValue
+
+
+#Vectors
+def VectorMagnitude(xy: tuple[float, float]) -> float:
+    return math.sqrt(xy[0]**2 + xy[1]**2)
+
+def VectorAngle(xy: tuple[float, float]) -> float:
+    return math.atan2(xy[0],xy[1])
+
+def VectorNew(angleNew: float, magnitudeNew: float) -> tuple[float, float]:
+    return (magnitudeNew * math.sin(angleNew), magnitudeNew * math.cos(angleNew))
+
+def VectorNormalize(v: tuple[float, float]) -> tuple[float, float]:
+    mag = VectorMagnitude(v)
+    return (v[0]/mag, v[1]/mag)
+
+def VectorMultiplyConstant(v: tuple[float, float], m: float) -> tuple[float, float]:
+    return (v[0] * m, v[1] * m)
+
+def VectorsAdd(v1: tuple[float, float], v2: tuple[float, float]) -> tuple[float, float]:
+    return (v1[0] + v2[0], v1[1] + v2[1])
